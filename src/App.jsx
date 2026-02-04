d99b07e50e9c436d5c4cafdb4f3e36e72c5f9fc2
Sania Joshi <156577110+Sania-joshi@users.noreply.github.com>
2026-02-04 14:34:10 +0530

    Fix casing issues for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Services from './pages/Services'
-import Contact from './pages/Contact'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Services from './Pages/Services'
+import Contact from './Pages/Contact'
 import AppNavbar from './components/AppNavbar'
 
 export default function App() {
