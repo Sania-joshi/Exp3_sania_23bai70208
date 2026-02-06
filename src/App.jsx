e558966ddb043cb70754d2ef05b6070b195f7d4c
Sania <priyajoshi70780@gmail.com>
2026-02-06 12:42:28 +0530

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,26 @@ import Home from './Pages/Home'
 import About from './Pages/About'
 import Services from './Pages/Services'
 import Contact from './Pages/Contact'
-import AppNavbar from './components/AppNavbar'
+import { Navbar, Nav, Container } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 export default function App() {
   return (
     <>
-      <AppNavbar />
+      <Navbar bg="primary" variant="dark" expand="lg" className="shadow">
+      <Container>
+        <Navbar.Brand as={Link} to="/" className="fw-bold">Experiment 3- Routing</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto fw-semibold">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/about">About</Nav.Link>
+            <Nav.Link as={Link} to="/services">Projects</Nav.Link>
+            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
